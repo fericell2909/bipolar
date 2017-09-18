2a4dccd8219202abe19c593b5481d84c44d295c5
Helmer Davila <helmer290192@hotmail.com>
2017-09-18 18:41:03 -0500

    All updated to Laravel 5.5

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,7 +1,9 @@
 <?php
 
+use Faker\Generator as Faker;
+
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(\App\Models\User::class, function (Faker\Generator $faker) {
+$factory->define(\App\Models\User::class, function (Faker $faker) {
     static $password;
 
     return [
