1b00fdb6e60a58a30008069a6a8cfe844c398e26
Helmer Davila <helmer290192@hotmail.com>
2017-10-30 02:09:43 -0500

    Subtype seeder

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -14,6 +14,7 @@ class ProductSeeder extends Seeder
         factory(\App\Models\Product::class, 20)->create(['active' => null]);
         factory(\App\Models\Product::class, 20)->create(['active' => now()])->each(function ($p) {
             /** @var \App\Models\Product $p */
+            $p->subtypes()->sync([array_random(range(1, 5))]);
             $p->stocks()->saveMany(factory(\App\Models\Stock::class, 3)->make(['product_id' => $p->id]));
             $p->photos()->saveMany(factory(\App\Models\Photo::class, 3)->make(['product_id' => $p->id]));
         });
