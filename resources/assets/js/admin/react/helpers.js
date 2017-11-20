5029333bc5b0e7f80a250f49d2e861a729a340b2
Helmer Davila <helmer290192@hotmail.com>
2017-11-20 03:36:14 -0500

    New method for helpers

--- resources/assets/js/admin/react/helpers.js
+++ resources/assets/js/admin/react/helpers.js
@@ -6,3 +6,15 @@ export function isEnterKey(event) {
 export function isLeftClick(event) {
     return event.button === 0;
 }
+
+export function existInArray(theArray, value) {
+    return theArray.findIndex(element => element === value) !== -1;
+}
+
+export function removeFromSimpleArray(theArray, value) {
+    const indexElement = theArray.findIndex(element => element === value);
+    if (indexElement !== -1) {
+        theArray.splice(indexElement, 1);
+    }
+    return [...theArray];
+}
