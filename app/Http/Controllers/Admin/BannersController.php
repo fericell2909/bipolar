582cc6c2b0f46660877d6b842036444fcb06181f
Helmer Davila <helmer290192@hotmail.com>
2018-01-02 13:59:01 -0500

    Add state to eager loading

--- app/Http/Controllers/Admin/BannersController.php
+++ app/Http/Controllers/Admin/BannersController.php
@@ -10,7 +10,7 @@ class BannersController extends Controller
 {
     public function index()
     {
-        $banners = Banner::orderBy('order')->get();
+        $banners = Banner::orderBy('order')->with('state')->get();
 
         return view('admin.banners.index', compact('banners'));
     }
