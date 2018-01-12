4caa0dc32426fbac04d7b26378c680d58913c69d
Helmer Davila <helmer290192@hotmail.com>
2018-01-12 18:55:48 -0500

    404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,8 @@
+@extends('web.layouts.app_web')
+@section('content')
+  <div class="page-404">
+    <h1>404 ERROR</h1>
+    <p>La página que estás buscando no existe</p>
+    <a href="/" class="btn btn-dark btn-rounded">Regresar al home</a>
+  </div>
+@endsection
