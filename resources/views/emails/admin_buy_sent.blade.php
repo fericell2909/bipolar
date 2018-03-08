1328d7beb964d8d5dcc8af4ad9675c08502d0116
Helmer Davila <helmer290192@hotmail.com>
2018-03-07 22:03:10 -0500

    Fix the email sent email

--- resources/views/emails/admin_buy_sent.blade.php
+++ resources/views/emails/admin_buy_sent.blade.php
@@ -9,27 +9,27 @@
         <table role="presentation" class="content_section" width="100%" border="0" cellspacing="0" cellpadding="0" style="box-sizing: border-box;mso-table-lspace: 0pt;mso-table-rspace: 0pt;border-spacing: 0;width: 100%;max-width: 640px;margin: 0 auto;text-align: center;min-width: 0 !important;">
           <tbody>
             <tr>
+              <td>
+                @if(\LaravelLocalization::getCurrentLocale() === 'es')
+                <img src="https://bipolar-peru.s3.amazonaws.com/assets/mails/pedido-enviado-esp.jpg" style="max-width: 100%" alt="Bipolar">
+                @elseif(\LaravelLocalization::getCurrentLocale() === 'en')
+                <img src="https://bipolar-peru.s3.amazonaws.com/assets/mails/pedido-enviado-eng.jpg" style="max-width: 100%" alt="Bipolar">
+                @endif
+              </td>
+            </tr>
+            <tr>
               <td class="content_cell content_b py tc" style="box-sizing: border-box;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;line-height: inherit;padding-left: 8px;padding-right: 8px;background-color: #ffffff;text-align: center;padding-top: 16px;padding-bottom: 16px;font-size: 0 !important;">
                 <table role="presentation" class="column" width="100%" border="0" cellspacing="0" cellpadding="0" style="box-sizing: border-box;mso-table-lspace: 0pt;mso-table-rspace: 0pt;border-spacing: 0;width: 100%;min-width: 0 !important;">
                   <tbody>
                     <tr>
                       <td class="column_cell pt tc" style="box-sizing: border-box;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;padding-left: 8px;padding-right: 8px;font-family: Arial, Helvetica, sans-serif;font-size: 16px;color: #757575;text-align: center;padding-top: 16px;line-height: inherit;">
-                        <table role="presentation" class="ic_h" align="center" width="80" border="0" cellspacing="0" cellpadding="0" style="box-sizing: border-box;mso-table-lspace: 0pt;mso-table-rspace: 0pt;border-spacing: 0;display: table;margin-left: auto;margin-right: auto;">
-                          <tbody>
-                            <tr>
-                              <td class="default_b" style="box-sizing: border-box;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: middle;background-color: #000000;line-height: 100%;padding: 16px;text-align: center;mso-line-height-rule: exactly;border-radius: 100px;">
-                                <img role="img" src="{{ asset('images/shopping-cart.svg') }}" width="48" height="48" alt="Heart symbol" style="max-width: 48px;outline: none;border: 0;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;line-height: 100%;">
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
                         <h1 style="font-family: Arial, Helvetica, sans-serif;font-weight: bold;padding: 0;margin-left: 0;margin-right: 0;margin-top: 16px;margin-bottom: 8px;color: #4d4d4d;font-size: 26px;line-height: 34px;">
-                          Tu producto ha sido enviado
+                          TU PEDIDO <br> HA SIDO ENVIADO
                         </h1>
-                        <p class="lead mb tm" style="font-family: Arial, Helvetica, sans-serif;font-size: 20px;color: #b3b3b3;line-height: 27px;mso-line-height-rule: exactly;margin-top: 0;margin-bottom: 16px;">
-                          Puede confirmar el estado de su pedido en el siguiente enlace
+                        <p class="lead mb tm" style="font-family: Arial, Helvetica, sans-serif;font-size: 20px;color: #000000;line-height: 27px;mso-line-height-rule: exactly;margin-top: 0;margin-bottom: 16px;">
+                          Puede confirmar el estado de su pedido aquí
                         </p>
-                        <table role="presentation" class="ebtn tc" align="center" border="0" cellspacing="0" cellpadding="0" style="box-sizing: border-box;mso-table-lspace: 0pt;mso-table-rspace: 0pt;border-spacing: 0;display: table;margin-left: auto;margin-right: auto;text-align: center;">
+                        <table role="presentation" class="ebtn tc" align="center" border="0" cellspacing="0" cellpadding="0" style="box-sizing: border-box;mso-table-lspace: 0pt;mso-table-rspace: 0pt;border-spacing: 0;display: table;margin-left: auto;margin-right: auto;text-align: center;margin-bottom: 20px;">
                           <tbody>
                             <tr>
                               <td class="accent_b" style="box-sizing: border-box;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;background-color: #000000;line-height: 22px;font-size: 18px;font-weight: bold;padding: 13px 24px;mso-line-height-rule: exactly;border-radius: 50px;text-align: center;">
