09ef4c323f08b0fa3bb053946ba71bc7a660e110
Helmer Davila <helmer290192@hotmail.com>
2018-03-09 01:22:57 -0500

    Newsletter mail

---
+++ app/Mail/NewsletterSuscribed.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class NewsletterSuscribed extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $name;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Gracias por suscribirte a nuestro newsletter')
+            ->view('emails.web_newsletter_suscribed');
+    }
+}
