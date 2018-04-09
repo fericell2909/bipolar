022509ae3e206b05b64299cdd80f3906ebbcf4e4
Helmer Davila <helmer290192@hotmail.com>
2018-04-08 21:12:10 -0500

    Fixing historic styles

--- resources/views/admin/historics/edit.blade.php
+++ resources/views/admin/historics/edit.blade.php
@@ -1,25 +1,28 @@
 @extends('admin.layouts.app_admin')
+<?php /** @var \App\Models\Historic $historic */ ?>
+@section('title', "Editar histórico {$historic->name}")
 @section('content')
-<div class="row">
-  <div class="col-md-12 white-box">
-      <h3 class="box-title">Editar histórico</h3>
+  <div class="card">
+    <div class="card-body">
       {!! Form::open(['files' => true]) !!}
-        <div class="form-row">
-            <div class="col-md-6 form-group">
-              {!! Form::label('Nombre') !!}
-              {!! Form::text('name', $historic->name, ['class' => 'form-control']) !!}
-            </div>
-            <div class="col-md-6 form-group">
-              {!! Form::label('Imagen (medidas: 794x527)') !!}
-              {!! Form::file('photo', ['class' => 'form-control']) !!}
-            </div>
+      <div class="form-row">
+        <div class="col-md-6 form-group">
+          {!! Form::label('Nombre') !!}
+          {!! Form::text('name', $historic->name, ['class' => 'form-control']) !!}
         </div>
-        {!! Form::submit('Guardar', ['class' => 'btn btn-dark btn-sm btn-rounded']) !!}
+        <div class="col-md-6 form-group">
+          {!! Form::label('Imagen (medidas: 794x527)') !!}
+          {!! Form::file('photo', ['class' => 'form-control']) !!}
+        </div>
+      </div>
+      {!! Form::submit('Guardar', ['class' => 'btn btn-dark btn-sm btn-rounded']) !!}
       {!! Form::close() !!}
+    </div>
   </div>
-  <div class="col-md-12 white-box">
-    <h3 class="box-title">Imagen actual</h3>
-    <img src="{{ $historic->photo }}">
+  <div class="card text-center">
+    <div class="card-body">
+      <h3 class="card-title">Imagen actual</h3>
+      <img src="{{ $historic->photo }}">
+    </div>
   </div>
-</div>
 @endsection
