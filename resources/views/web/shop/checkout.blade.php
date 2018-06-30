bc9f001037e59dba463b4f01b323d0a8b0e0aefc
Helmer Davila <helmer290192@hotmail.com>
2018-06-30 04:29:42 -0500

    Removed zip field

--- resources/views/web/shop/checkout.blade.php
+++ resources/views/web/shop/checkout.blade.php
@@ -186,7 +186,7 @@
 								</div>
 								<div class="form-group col-md-6">
 									{!! Form::label(__('bipolar.form_fields.zip')) !!}
-									{!! Form::text('zip', null, ['class' => 'form-control', 'required' => true, 'autocomplete' => 'off']) !!}
+									{!! Form::text('zip', null, ['class' => 'form-control', 'autocomplete' => 'off']) !!}
 								</div>
 							</div>
               <div class="text-center">
