8c4b4b70cdef62b1902c98d7d2e309d22019c562
Helmer Davila <helmer290192@hotmail.com>
2018-07-25 00:50:02 -0500

    Fixing the facebook counter

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -74,7 +74,7 @@
                             <div class="bipolar-counts-subtitle">Instagram fans</div>
                         </div>
                         <div class="col-md-4 bipolar-counts">
-                            <div id="bipolar-second-counter" class="bipolar-counts-title"></div>
+                        <div id="bipolar-second-counter" class="bipolar-counts-title" data-number="{{ $settings->facebook_counts }}"></div>
                             <div class="bipolar-counts-subtitle">Facebook Fans</div>
                         </div>
                     </div>
