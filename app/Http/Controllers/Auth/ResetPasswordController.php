05decedec24fa99cb8f66e8bba037eb4e22e863a
Helmer Davila <helmer290192@hotmail.com>
2018-07-30 20:58:13 -0500

    Fix 404 after reset password

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -26,7 +26,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/my-account';
+    protected $redirectTo = '/my-account/buys';
 
     /**
      * Create a new controller instance.
