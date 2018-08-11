f8503e001b8de487c7a0a594df56138afd1e5ee6
Helmer <helmer290192@hotmail.com>
2018-08-11 00:08:44 -0500

    Change route for photos

--- app/Console/Commands/MoveS3PhotoLinks.php
+++ app/Console/Commands/MoveS3PhotoLinks.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Banner;
 use Illuminate\Console\Command;
 use App\Models\Photo;
 
@@ -41,8 +42,15 @@ class MoveS3PhotoLinks extends Command
         $photos = Photo::whereNotNull('url')->get();
 
         $photos->each(function ($photo) {
-            $photo->url = str_replace('https://s3.amazonaws.com/bipolar-peru', env('APP_URL') . '/bipolar-images', $photo->url);
+            $photo->url = str_replace('https://s3.amazonaws.com/bipolar-peru', env('APP_URL') . 'storage/bipolar-images', $photo->url);
             $photo->save();
+        });
+
+        $banners = Banner::all();
+
+        $banners->each(function ($banner) {
+            $banner->url = str_replace('https://s3.amazonaws.com/bipolar-peru', env('APP_URL') . 'storage/bipolar-images', $banner->url);
+            $banner->save();
         });
 
         $this->info('Changed');
