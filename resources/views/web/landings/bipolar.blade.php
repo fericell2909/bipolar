1f3c084b137beff27f0d952e1939a5918b1df1dc
Helmer <helmer290192@hotmail.com>
2018-09-10 00:08:14 -0500

    Add new header to bipolar page

--- resources/views/web/landings/bipolar.blade.php
+++ resources/views/web/landings/bipolar.blade.php
@@ -1,22 +1,23 @@
 @extends('web.layouts.app_web')
 @section('content')
-    <img src="{{ asset('storage/bipolar-images/assets/jeringas-rosado.jpg') }}" style="width: 100%;" class="img-responsive" alt="Bipolar">
-    <div class="bipolar-page-container">
-        <h1>BIPOLAR</h1>
-        <p>
-            Nos encanta la dualidad y que no todo sea lo que parece. <br>
-            Nos gusta la línea que divide lo femenino y dulce, de lo secreto y provocador. <br>
-            Queremos tener el control pero jugamos a perderlo. <br>
-            Nos gusta lo cursi y lo sobrio. Lo correcto y lo indebido. <br>
-            Nos gusta ir de rosa. Pero, sobre todo, nos gusta ir de negro. <br>
-        </p>
-        <p>
-            Nuestra pasión son los tacos altos y lo que nos hacen sentir. <br>
-            Desde el 2011, Bipolar diseña zapatos de cuero para que seas quien quieras ser, <br>
-            para jugar o conquistar el mundo. <br>
-        </p>
-        <p>
-            <a class="special-link" href="{{ route('landings.historico') }}">Aquí</a> un recorrido por todos nuestros bipolares desde el inicio!
-        </p>
-    </div>
+  <img src="{{ asset('storage/bipolar-images/assets/bipolar.png') }}" style="width: 100%;" class="img-responsive" alt="Bipolar">
+  <div class="bipolar-page-container">
+    <h1>BIPOLAR</h1>
+    <p>
+      Nos encanta la dualidad y que no todo sea lo que parece. <br>
+      Nos gusta la línea que divide lo femenino y dulce, de lo secreto y provocador. <br>
+      Queremos tener el control pero jugamos a perderlo. <br>
+      Nos gusta lo cursi y lo sobrio. Lo correcto y lo indebido. <br>
+      Nos gusta ir de rosa. Pero, sobre todo, nos gusta ir de negro. <br>
+    </p>
+    <p>
+      Nuestra pasión son los tacos altos y lo que nos hacen sentir. <br>
+      Desde el 2011, Bipolar diseña zapatos de cuero para que seas quien quieras ser, <br>
+      para jugar o conquistar el mundo. <br>
+    </p>
+    <p>
+      <a class="special-link" href="{{ route('landings.historico') }}">Aquí</a> un recorrido por todos nuestros
+      bipolares desde el inicio!
+    </p>
+  </div>
 @endsection
