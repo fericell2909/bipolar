53f81a5dff08497c5a363ee51f322b88316ccab6
Helmer <helmer290192@hotmail.com>
2018-09-23 16:38:38 -0500

    Go to list of buys in mobile

--- resources/views/web/auth/myaccount.blade.php
+++ resources/views/web/auth/myaccount.blade.php
@@ -10,34 +10,36 @@
     </p>
     {!! Form::open(['route' => 'logout', 'style' => 'display:none', 'id' => 'logout-form']) !!}
     {!! Form::close() !!}
-    <table class="table-buys">
-      <thead>
-        <tr>
-          <th>{{ __('bipolar.my_account.order') }}</th>
-          <th>{{ __('bipolar.my_account.date') }}</th>
-          <th>{{ __('bipolar.my_account.state') }}</th>
-          <th>Total</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach($buys as $buy)
-        <tr>
-          <td><a href="{{ route('confirmation', $buy->id) }}" class="order-link">#{{ $buy->id }}</a></td>
-          <td>{{ $buy->created_at->format('d-m-Y') }}</td>
-          <td>{{ $buy->status }}</td>
-          <td><span class="price">{{ $buy->totalCurrency }}</span> por {{ $buy->details->count() }} items</td>
-          <td class="order-actions">
-            @if($buy->status === config('constants.BUY_INCOMPLETE_STATUS'))
-              <button class="btn btn-dark-rounded bipolar-delete-buy" data-confirmation="{{ __('bipolar.buy.delete_question') }}" data-buy-hash-id="{{ $buy->hash_id }}">
-                {{ __('bipolar.my_account.cancel') }}
-              </button>
-            @endif
-            <a href="{{ route('confirmation', $buy->id) }}" class="btn btn-dark-rounded">{{ __('bipolar.my_account.view') }}</a>
-          </td>
-        </tr>
-        @endforeach
-      </tbody>
-    </table>
+    <div class="table-responsive">
+      <table class="table-buys">
+        <thead>
+          <tr>
+            <th>{{ __('bipolar.my_account.order') }}</th>
+            <th>{{ __('bipolar.my_account.date') }}</th>
+            <th>{{ __('bipolar.my_account.state') }}</th>
+            <th>Total</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($buys as $buy)
+            <tr>
+              <td><a href="{{ route('confirmation', $buy->id) }}" class="order-link">#{{ $buy->id }}</a></td>
+              <td>{{ $buy->created_at->format('d-m-Y') }}</td>
+              <td>{{ $buy->status }}</td>
+              <td><span class="price">{{ $buy->totalCurrency }}</span> por {{ $buy->details->count() }} items</td>
+              <td class="order-actions">
+                @if($buy->status === config('constants.BUY_INCOMPLETE_STATUS'))
+                  <button class="btn btn-dark-rounded bipolar-delete-buy" data-confirmation="{{ __('bipolar.buy.delete_question') }}" data-buy-hash-id="{{ $buy->hash_id }}">
+                    {{ __('bipolar.my_account.cancel') }}
+                  </button>
+                @endif
+                <a href="{{ route('confirmation', $buy->id) }}" class="btn btn-dark-rounded">{{ __('bipolar.my_account.view') }}</a>
+              </td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
   </div>
 @endsection
--- resources/views/web/partials/mobile-bar.blade.php
+++ resources/views/web/partials/mobile-bar.blade.php
@@ -18,7 +18,7 @@
     <i class="fa fa-chevron-down"></i>
   </div>
   <ul class="bipolar-dropdown-menu in-mobile hidden-md hidden-lg">
-    <li><a href="{{ route('profile') }}"><i class="fa fa-fw fa-user"></i> {{ __('bipolar.navbar.my_account') }}</a></li>
+    <li><a href="{{ route('myaccount') }}"><i class="fa fa-fw fa-user"></i> {{ __('bipolar.navbar.my_account') }}</a></li>
     <li><a href="{{ route('wishlist') }}"><i class="fa fa-fw fa-heart"></i> Wishlist</a></li>
     <li><a href="{{ route('cart') }}"><i class="fa fa-fw fa-shopping-cart"></i> Shopping cart</a></li>
     <li><a href="{{ route('checkout') }}"><i class="fa fa-fw fa-share"></i> Checkout</a></li>
@@ -34,7 +34,7 @@
       </li>
     @endforeach
     @auth
-      <li><a href="#">Cerrar sesión</a></li>
+      <li><a href="#">{{ __('bipolar.navbar.logout') }}</a></li>
     @endauth
   </ul>
 </section>
