9006da1c7a2769ec05ce1c8d3bb5f8874594b277
Helmer <helmer290192@hotmail.com>
2018-10-21 18:06:10 -0500

    Activate home post 10 minutes after midnight

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\ExecuteDiscountTasks;
+use App\Console\Commands\HomePostActivation;
 use App\Console\Commands\RevertDiscountTasks;
 use App\Console\Commands\SendNoBuyedCarts;
 use App\Console\Commands\SyncBsaleStocks;
@@ -12,6 +13,8 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    private $tenMinutesAfterMidnight = '00:10';
+
     /**
      * The Artisan commands provided by your application.
      *
@@ -29,12 +32,13 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(ExecuteDiscountTasks::class)->daily();
-        $schedule->command(RevertDiscountTasks::class)->daily();
-        //$schedule->command(SendNoBuyedCarts::class)->daily();
-        $schedule->command(CopyFacebookFansToSettings::class)->daily();
+        $schedule->command(ExecuteDiscountTasks::class)->dailyAt($this->tenMinutesAfterMidnight);
+        $schedule->command(RevertDiscountTasks::class)->dailyAt($this->tenMinutesAfterMidnight);
+        $schedule->command(CopyFacebookFansToSettings::class)->dailyAt($this->tenMinutesAfterMidnight);
+        $schedule->command(HomePostActivation::class)->dailyAt($this->tenMinutesAfterMidnight);
         $schedule->command(SyncBsaleStocks::class)->everyMinute();
         $schedule->command('sitemap:generate')->weekly();
+        //$schedule->command(SendNoBuyedCarts::class)->daily();
     }
 
     /**
