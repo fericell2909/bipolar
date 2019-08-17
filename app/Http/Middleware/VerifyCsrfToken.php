27e310dcede38c28b15e672bdf4ca025064ef797
Helmer <helmer290192@hotmail.com>
2019-08-16 21:14:32 -0500

    Added stock update endpoint

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        '/confirmation-payment'
+        '/confirmation-payment',
+        '/bsale/sync'
     ];
 }
