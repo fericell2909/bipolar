ef1cee18e892a73ef0ddcb8055a865a31dc3e039
Helmer <helmer290192@hotmail.com>
2019-08-25 22:13:59 -0500

    Installing Vessel

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2019-08-02 17:42:14.
+    * Generated on 2019-08-25 22:09:38.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.25 on 2019-08-02 17:42:13.
+ * Generated for Laravel 5.7.25 on 2019-08-25 22:09:38.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
