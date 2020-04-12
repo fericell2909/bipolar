51714e85a22e845f801d4b166a2ed69a65f2a53e
Helmer <helmer290192@hotmail.com>
2020-04-12 17:17:08 -0500

    Apply 2x1 task

--- app/Console/Commands/RevertDiscountTasks.php
+++ app/Console/Commands/RevertDiscountTasks.php
@@ -55,34 +55,11 @@ class RevertDiscountTasks extends Command
 
         $discountTasks->each(function ($discount) {
             /** @var DiscountTask $discount */
-            $mainParams = [
-                'discount_pen'   => null,
-                'discount_usd'   => null,
-                'begin_discount' => null,
-                'end_discount'   => null,
-            ];
-
-            if ($discount->product_types) {
-                $types = Type::find($discount->product_types);
-                foreach ($types as $type) {
-                    foreach ($type->subtypes as $subtype) {
-                        $subtype->products->each($this->assignMassiveDiscount(0, 0, $mainParams, true));
-                    }
-                }
+            if ($discount->is_2x1) {
+                $discount = $this->remove2x1Discount($discount);
+            } else {
+                $discount = $this->removeCurrencyDiscount($discount);
             }
-
-            if ($discount->product_subtypes) {
-                $subtypes = Subtype::find($discount->product_subtypes);
-                foreach ($subtypes as $subtype) {
-                    $subtype->products->each($this->assignMassiveDiscount(0, 0, $mainParams, true));
-                }
-            }
-
-            if ($discount->products) {
-                $products = Product::find($discount->products);
-                $products->each($this->assignMassiveDiscount(0, 0, $mainParams, true));
-            }
-
             $discount->executed = false;
             $discount->save();
         });
@@ -92,7 +69,74 @@ class RevertDiscountTasks extends Command
         }
     }
 
-    private function assignMassiveDiscount($discountPEN, $discountUSD, $mainParams, $convertToNull)
+    private function remove2x1Discount(DiscountTask $discount): DiscountTask
+    {
+        if ($discount->product_types) {
+            $types = Type::find($discount->product_types);
+            foreach ($types as $type) {
+                foreach ($type->subtypes as $subtype) {
+                    $subtype->products->each($this->productMassive2x1Update());
+                }
+            }
+        }
+
+        if ($discount->product_subtypes) {
+            $subtypes = Subtype::find($discount->product_subtypes);
+            foreach ($subtypes as $subtype) {
+                $subtype->products->each($this->productMassive2x1Update());
+            }
+        }
+
+        if ($discount->products) {
+            $products = Product::find($discount->products);
+            $products->each($this->productMassive2x1Update());
+        }
+
+        return $discount;
+    }
+
+    private function removeCurrencyDiscount(DiscountTask $discount): DiscountTask
+    {
+        $mainParams = [
+            'discount_pen'   => null,
+            'discount_usd'   => null,
+            'begin_discount' => null,
+            'end_discount'   => null,
+        ];
+
+        if ($discount->product_types) {
+            $types = Type::find($discount->product_types);
+            foreach ($types as $type) {
+                foreach ($type->subtypes as $subtype) {
+                    $subtype->products->each($this->productMassiveDiscountUpdate(0, 0, $mainParams, true));
+                }
+            }
+        }
+
+        if ($discount->product_subtypes) {
+            $subtypes = Subtype::find($discount->product_subtypes);
+            foreach ($subtypes as $subtype) {
+                $subtype->products->each($this->productMassiveDiscountUpdate(0, 0, $mainParams, true));
+            }
+        }
+
+        if ($discount->products) {
+            $products = Product::find($discount->products);
+            $products->each($this->productMassiveDiscountUpdate(0, 0, $mainParams, true));
+        }
+
+        return $discount;
+    }
+
+    private function productMassive2x1Update()
+    {
+        return function ($product) {
+            /** @var Product $product */
+            return $product->update(['is_deal_2x1' => false]);
+        };
+    }
+
+    private function productMassiveDiscountUpdate($discountPEN, $discountUSD, $mainParams, $convertToNull)
     {
         return function ($product) use ($discountPEN, $discountUSD, $mainParams, $convertToNull) {
             /** @var Product $product */
