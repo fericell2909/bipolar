1146a1d4731ab1590c4324116ebe24658b4947ae
Helmer <helmer290192@hotmail.com>
2020-06-20 22:35:08 -0500

    Allow banner editing and centering content

--- resources/views/web/partials/banner-colors.blade.php
+++ resources/views/web/partials/banner-colors.blade.php
@@ -1,9 +1,11 @@
 <div class="owl-carousel owl-carousel-banner-colors">
   @foreach($bannerColors as $bannerColor)
-    <a class="owl-carousel-banner-colors-item"
-       href="{{ $bannerColor->link }}"
-       style="background-color: {{ $bannerColor->background_color }};
-           color: {{ $bannerColor->color }};">
+    <a class="owl-carousel-banner-colors-item" href="{{ $bannerColor->link }}"
+       style="
+           background-color: {{ $bannerColor->background_color }};
+           color: {{ $bannerColor->color }};
+           text-align: center;
+           ">
       {!! $bannerColor->text !!}
     </a>
   @endforeach
