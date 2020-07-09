0a568a604a234edd6565e208e619f507c5ebad0c
Helmer <helmer290192@hotmail.com>
2020-07-08 20:33:29 -0500

    Autogenerate UUID

--- app/Models/Fit.php
+++ app/Models/Fit.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Dyrynda\Database\Support\GeneratesUuid;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Translatable\HasTranslations;
 
 class Fit extends Model
 {
-    use HasTranslations;
+    use HasTranslations, GeneratesUuid;
 
     public $timestamps = false;
     public $translatable = ['name'];
