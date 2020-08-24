eba6e733e30783e58c77dcd1c1b305e5e9114699
Helmer <helmer290192@hotmail.com>
2020-08-24 12:54:35 -0500

    Fixing some JS and UI issues with the desktop cart

---
+++ resources/assets/js/web/click-outside-handler.ts
@@ -0,0 +1,24 @@
+$(document).on('click ', function(event) {
+  if (
+    !$(event.target).is('.cart-white-mobile > img') &&
+    !$(event.target).is('.cart-white-mobile > span')
+  ) {
+    $('.cart-inside-mobile').css('display', 'none');
+  }
+
+  if (
+    !$(event.target).is('.bipolar-shopping-cart-content > img') &&
+    !$(event.target).is('.bipolar-shopping-cart-content > span')
+  ) {
+    $('.cart-inside').css('display', 'none');
+  }
+
+  if (
+    !$(event.target).is('.text-heading-account') &&
+    !$(event.target).is('.text-heading-account > i') &&
+    !$(event.target).is('.navbar-right-text') &&
+    !$(event.target).is('.navbar-right-text > i')
+  ) {
+    $('.bipolar-dropdown-menu').css('display', 'none');
+  }
+});
