5b3bdb826e93b8f00abe2da8c7201aa041f8ba91
Helmer <helmer290192@hotmail.com>
2020-10-26 23:27:20 -0500

    Moving pixel from Google Tag Manager to the project (#20)
    * Moving Facebook Pixel out of the tag manager
    
    * Some VSCode formatting

---
+++ resources/views/web/partials/facebook-pixel.blade.php
@@ -0,0 +1,21 @@
+<!-- Facebook Pixel Code -->
+<meta property="fb:app_id" content="100210840716931">
+<script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '734891250285490');
+    fbq('track', 'PageView');
+</script>
+@stack('facebook_pixel_event')
+<noscript>
+    <img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=734891250285490&ev=PageView&noscript=1" />
+</noscript>
+<!-- End Facebook Pixel Code -->
+{{ debug(Route::currentRouteName()) }}
