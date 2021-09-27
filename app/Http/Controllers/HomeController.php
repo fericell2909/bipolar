3474a58d850bbc6c8b67b80a58cb9f045a2cf45e
root <info@devmarcoestrada.com>
2021-09-27 18:24:40 -0500

    message

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,16 +13,17 @@ class HomeController extends Controller
     {
         /** @var Page $page */
         $page = Page::findBySlugOrFail($pageSlug);
-        /*
+        
         if ($page->main_image) {
             $this->seo()->opengraph()->setType('article')->addImage($page->main_image);
             $this->seo()->twitter()->addImage($page->main_image);
         } else {
-            $imageUrl = asset('storage/bipolar-images/assets/jeringas-rosado.jpg');
+            //$imageUrl = asset('storage/bipolar-images/assets/jeringas-rosado.jpg');
+            $imageUrl = '';
             $this->seo()->opengraph()->addImage($imageUrl);
             $this->seo()->twitter()->addImage($imageUrl);
         }
-        */
+        
         return view('web.landings.pages', compact('page'));
     }
 }
